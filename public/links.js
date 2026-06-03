fa9a016c6b6d743ed7307f72ec43caeb6dd0113f
Jaegyu <JaegyuDev@proton.me>
2026-06-02 20:53:45 -0400

    fixed index

--- public/links.js
+++ public/links.js
@@ -1,5 +1,5 @@
 const links = [
-    { slug: '/', url: '/index.html' },
+    { slug: '/', url: '/' },
     { slug: '/projects', url: '/projects.html' },
     { slug: '/contact-me', url: '/contact-me.html' },
     { slug: '/github', url: 'https://github.com/JaegyuDev' },
